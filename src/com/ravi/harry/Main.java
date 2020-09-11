667cfa13ce3a177163c82e0bbdbae64d1a45efe2
Ravi Lohana <ravilohana@users.noreply.github.com>
2020-09-11 21:03:17 +0530

    test pull command

--- src/com/ravi/harry/Main.java
+++ src/com/ravi/harry/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("GitHub ");
         System.out.println("Thsi is test push command");
+        System.out.println("Thsi is test pull  command from github");
+        sout
 
     }
 }
